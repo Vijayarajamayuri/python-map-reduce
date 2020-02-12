b406b539020bbf54ed37d7435f713efd15cefb7e
Vijayarajamayuri <avrmayuri74@gmail.com>
2020-02-12 13:49:14 -0600

    commmit

--- 1mapper.py
+++ 1mapper.py
@@ -1,10 +1,10 @@
-input = open("d.txt", "r")
+input = open("purchases.txt", "r")
 output = open("01.txt", "w")
 
 for line in input:
     datalist = line.strip().split("    ")
-    date, time,store, item, cost, paymentType = datalist
-    output.write(store + "\t" + cost + "\n")
+    date, time,store, item, cost, purchaseType = datalist
+    output.write(purchaseType + "\t" + item + "\n")
 
 input.close()
 output.close()
--- 3reducer.py
+++ 3reducer.py
@@ -1,24 +1,25 @@
 s = open("02.txt","r")
 r = open("03.txt", "w")
 
+
 thisKey = ""
 thisValue = 0.0
 
 for line in s:
   data = line.strip().split('\t')
-  store, amount = data
+  purchaseType, count = data
 
-  if store != thisKey:
+  if purchaseType != thisKey:
     if thisKey:
       # output the last key value pair result
       r.write(thisKey + '\t' + str(thisValue)+'\n')
 
     # start over when changing keys
-    thisKey = store 
+    thisKey = purchaseType 
     thisValue = 0.0
   
   # apply the aggregation function
-  thisValue += float(amount)
+  thisValue += 1
 
 # output the final entry when done
 r.write(thisKey + '\t' + str(thisValue)+'\n')
